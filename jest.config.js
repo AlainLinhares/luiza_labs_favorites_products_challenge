f6edde50a5df63de5aab316152f14a5744863b3f
Alain Melo <arllmcomputacao@gmail.com>
2025-09-17 15:36:16 -0300

    chore: add initial project config files

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test/unit'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {'^.+\\.(t|j)s$': 'ts-jest'},
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
